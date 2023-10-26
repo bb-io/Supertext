766cb2ee0cc0ce92ed4209c24f2581cd2c5af8f3
sokolenkoolha <olha.sokolenko@blackbird.io>
2023-10-26 13:07:54 +0300

    preparations for the artifactory

---
+++ Apps.Supertext/Connections/ConnectionValidator.cs
@@ -0,0 +1,35 @@
+﻿using Apps.Supertext.Api;
+using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Connections;
+using RestSharp;
+
+namespace Apps.Supertext.Connections;
+
+public class ConnectionValidator : IConnectionValidator
+{
+    public async ValueTask<ConnectionValidationResponse> ValidateConnection(
+        IEnumerable<AuthenticationCredentialsProvider> authenticationCredentialsProviders,
+        CancellationToken cancellationToken)
+    {
+        var client = new SupertextClient();
+        var request = new SupertextRequest("/v1/accountcheck", Method.Get, authenticationCredentialsProviders);
+        
+        try
+        {
+            await client.ExecuteWithErrorHandling(request);
+            return new ConnectionValidationResponse
+            {
+                IsValid = true,
+                Message = "Success"
+            };
+        }
+        catch (Exception)
+        {
+            return new ConnectionValidationResponse
+            {
+                IsValid = false,
+                Message = "Invalid email or API token"
+            };
+        }
+    }
+}
