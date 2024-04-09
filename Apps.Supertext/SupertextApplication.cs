dc9cad4f5485af82f0fd7d894fbb6994ada555c8
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-09 15:44:41 +0300

    added categories

--- Apps.Supertext/SupertextApplication.cs
+++ Apps.Supertext/SupertextApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.Supertext;
 
-public class SupertextApplication : IApplication
+public class SupertextApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.LspPortal];
+        set { }
+    }
+    
     public string Name
     {
         get => "Supertext";
